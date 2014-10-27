4bbb1cf8c3077fc5b656ba42f85ff6ec90eee4cc
野村修 <hugo-sb@ofellabuta.local>
2014-10-27 23:22:12 +0900

    Initial Commit

---
+++ Hillegass-ch2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hillegass-ch2
+//
+//  Created by 野村修 on 2014/10/27.
+//  Copyright (c) 2014年 ofellabuta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
